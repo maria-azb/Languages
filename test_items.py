1c6749f2f3746a4ddb49cdcbb672513e72e2248e
Maria Azbeleva <azbeleva@gmail.com>
2020-04-21 12:32:13 +0300

    fix name

--- test_items.py
+++ test_items.py
@@ -2,9 +2,9 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"	
 
-def test_items(browser):
+def test_button(browser):
     browser.get(link)
-    time.sleep(30)
+    time.sleep(10)
     browser.find_element_by_css_selector(".btn-add-to-basket")
     assert True, "Some Error"
 	
