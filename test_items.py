33cd7164f8eedfc606fac83bfe0be6c41bb67426
Maria Azbeleva <azbeleva@gmail.com>
2020-04-26 12:28:46 +0300

    Modified assert, change test name

--- test_items.py
+++ test_items.py
@@ -1,10 +1,15 @@
 import time
+from selenium.common.exceptions import NoSuchElementException
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"	
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_button(browser):
+def test_button_add_to_basket_present(browser):
     browser.get(link)
     time.sleep(10)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert True, "Some Error"
+    try:	
+        browser.find_element_by_css_selector(".btn-add-to-basket")
+        is_button = True
+    except (NoSuchElementException):
+        is_button = False    	
+    assert is_button == True, "Button 'add to basket' wasn't found"
 	
